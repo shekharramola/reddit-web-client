110d4476ed6033e43a76d3454add949956de7250
shekhar <shekharramola2012@gmail.com>
2019-12-27 10:07:29 +0530

    Updates

---
+++ precache-manifest.c2b7aa9821f361cd39bc1649c4142fe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68baaefa4f32ca202e17fbcc8ad3a468",
+    "url": "/reddit-web-client/index.html"
+  },
+  {
+    "revision": "724e304c86bc4f752033",
+    "url": "/reddit-web-client/static/css/main.8808fce7.chunk.css"
+  },
+  {
+    "revision": "6fbe41d6aad9a950e013",
+    "url": "/reddit-web-client/static/js/2.6dfc7ee7.chunk.js"
+  },
+  {
+    "revision": "020f2d8769602970321405d95c557731",
+    "url": "/reddit-web-client/static/js/2.6dfc7ee7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "724e304c86bc4f752033",
+    "url": "/reddit-web-client/static/js/main.ba468ea5.chunk.js"
+  },
+  {
+    "revision": "8b39146f4457db1c35be",
+    "url": "/reddit-web-client/static/js/runtime-main.245d4ff7.js"
+  },
+  {
+    "revision": "b208df44f3c8af1bf1fd8d1baabb6d5a",
+    "url": "/reddit-web-client/static/media/vector.b208df44.png"
+  }
+]);
